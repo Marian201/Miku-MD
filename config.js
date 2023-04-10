775378ac22195c52456c72c87b1d8aaff5457cfe
Marian201 <128308673+Marian201@users.noreply.github.com>
2023-04-10 15:22:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['4915750935328']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['4915750935328']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['4915750935328']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.BotName = "HEX"
+global.packname = "HEX BOT"
+global.author = "By: Marian"
+global.OwnerName = "Marian"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
